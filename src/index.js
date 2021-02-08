377a686710d7a264e26450d6733728baefca19eb
4forces <62040770+4forces@users.noreply.github.com>
2021-02-08 23:13:16 +0800

    Add 'xIsNext' to flip 'O' and 'X'

--- src/index.js
+++ src/index.js
@@ -28,13 +28,17 @@ import './index.css';
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; /* if xIsNext = True, results in 'X', else results in 'O' */
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
